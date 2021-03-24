d8dbe652fc3032ebbcf66c3d73b7cfa76873aa0b
Oleksandra <oleksatu@gmail.com>
2021-03-24 19:38:22 +0200

    feat: add vigenere cipher task

--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -1,13 +1,62 @@
 const CustomError = require("../extensions/custom-error");
 
 class VigenereCipheringMachine {
-  encrypt() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+    constructor(arg){
+        this.arg = arg;
+    }
+  encrypt(str, key) {
+    if(str == undefined || key == undefined){
+      throw new Error("THROWN");
+    }
+       
+    var arr = str.split("");
+    var key_arr = key.split("");
+    var res_arr = [];
+    var j = 0;
+      
+    for(var i = 0; i < arr.length; i++){
+      if(/[A-Za-z]/.test(arr[i])){
+        res_arr[i] = String.fromCharCode(((arr[i].toUpperCase().charCodeAt(0)-65) + (key_arr[j%key_arr.length].toUpperCase().charCodeAt(0)-65))%26 + 65);
+        j++;
+      }
+      else{
+        res_arr[i] = arr[i];
+      }
+    }
+      
+      if(this.arg == undefined || this.arg == true){
+        return res_arr.join("");
+      }
+      else if(this.arg == false){
+        return res_arr.reverse().join("");
+      }
   }    
-  decrypt() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+  decrypt(str, key) {
+      if(str == undefined || key == undefined){
+      throw new Error("THROWN");
+    }
+       
+    var arr = str.split("");
+    var key_arr = key.split("");
+    var res_arr = [];
+    var j = 0;
+      
+    for(var i = 0; i < arr.length; i++){
+      if(/[A-Za-z]/.test(arr[i])){
+        res_arr[i] = String.fromCharCode(( (arr[i].toUpperCase().charCodeAt(0)-65) - (key_arr[j%key_arr.length].toUpperCase().charCodeAt(0)-65) +26)%26 + 65);
+        j++;
+      }
+      else{
+        res_arr[i] = arr[i];
+      }
+    }
+      
+      if(this.arg == undefined || this.arg == true){
+        return res_arr.join("");
+      }
+      else if(this.arg == false){
+        return res_arr.reverse().join("");
+      }
   }
 }
 
